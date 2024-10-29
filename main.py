06fd0f199879f50414f93f3addb73b544730ba1c
SlaX0riuZ <slaxoriuz@gmail.com>
2024-10-29 10:50:53 -0500

    fetch user's input

---
+++ main.py
@@ -0,0 +1,9 @@
+import math
+
+# get a single string of input from user
+n_c = input("Enter two numbers to compare, separated by a space: ")
+
+n_list = n_c.split() # turns the string into the two numbers
+
+# n_list[0] is the user's first number
+# n_list[1] is the user's second number
